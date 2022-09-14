c970ba619e4688c1c91b4335431595acb671205a
Supriya Rajkoomar <supriyarajkoomar@gmail.com>
2022-09-14 17:28:01 +0400

    Proper variable name for unit test

--- CodeChallengeTests/CodeChallengeTests.swift
+++ CodeChallengeTests/CodeChallengeTests.swift
@@ -9,21 +9,21 @@ import XCTest
 @testable import CodeChallenge
 
 final class CodeChallengeTests: XCTestCase {
-    var sut: FeedViewModel?
+    var viewModel: FeedViewModel?
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
         try super.setUpWithError()
-        sut = FeedViewModel()
+        viewModel = FeedViewModel()
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
-        sut = nil
+        viewModel = nil
     }
     
     func testDataString() {
-        let answer = sut?.getDate(WithString: "2009-12-21T00:00:00Z", format: "dd MMM, yyyy")
+        let answer = viewModel?.getDate(WithString: "2009-12-21T00:00:00Z", format: "dd MMM, yyyy")
         XCTAssertEqual(answer, "21 Dec, 2009")
     }
 
