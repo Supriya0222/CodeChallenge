b20327faea420581e1b704bd3d4604cdc69bdd41
Supriya Rajkoomar <supriyarajkoomar@gmail.com>
2022-09-14 14:18:39 +0400

    CodeChallenge updated

--- CodeChallenge/View Controllers/NasaListViewController.swift
+++ CodeChallenge/View Controllers/NasaListViewController.swift
@@ -36,8 +36,6 @@ class NasaListViewController: UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
-        navigationItem.title = ""
-
     }
     
     override func viewDidAppear(_ animated: Bool) {
